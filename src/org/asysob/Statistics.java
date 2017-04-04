8a54dc765506aad76669df2ce63505b27d820343
Peter Sturm <sturm@uni-trier.de>
2017-04-04 12:31:07 +0200

    Some checks and comfort functions added

--- src/org/asysob/Statistics.java
+++ src/org/asysob/Statistics.java
@@ -2,18 +2,24 @@ package org.asysob;
 
 public class Statistics {
 
-    public Statistics () {
+    public Statistics(int skip) {
         n = 0;
         sum = 0.0;
         min = Double.MAX_VALUE;
         max = Double.MIN_VALUE;
+        this.skip = skip;
+        startHappened = false;
     }
 
-    public void StartMeasure () {
+    public void StartMeasure() {
         t_start = System.nanoTime();
+        startHappened = true;
     }
 
-    public void StopMeasure () {
+    public void StopMeasure() {
+        assert (startHappened);
+        startHappened = false;
+        if (skip-- > 0) return;
         t_stop = System.nanoTime();
         double d = Duration();
         sum += d;
@@ -22,20 +28,37 @@ public class Statistics {
         if (d > max) max = d;
     }
 
-    public double Duration () {
+    public double Duration() {
+        assert (startHappened);
         return (((double) t_stop) - ((double) t_start)) / 1000.0 / 1000.0;
     }
 
-    public String Report () {
+    public long NumberOfMeasures() {
+        return n;
+    }
+
+    public double AverageDuration() {
+        return sum / ((double) n);
+    }
+
+    public double MinimumDuration() {
+        return min;
+    }
+
+    public double MaximumDuration() {
+        return max;
+    }
+
+    public String Report() {
         StringBuilder s = new StringBuilder();
         s.append("n=");
         s.append(n);
         s.append(", min=");
-        s.append(String.format("%.4E",min));
+        s.append(String.format("%.4E", min));
         s.append(", average=");
-        s.append(String.format("%.4E",sum/((double) n)));
+        s.append(String.format("%.4E", AverageDuration()));
         s.append(", max=");
-        s.append(String.format("%.4E",max));
+        s.append(String.format("%.4E", max));
         return s.toString();
     }
 
@@ -46,4 +69,7 @@ public class Statistics {
     private double sum;
     private double min;
     private double max;
+
+    private int skip;
+    private boolean startHappened;
 }
